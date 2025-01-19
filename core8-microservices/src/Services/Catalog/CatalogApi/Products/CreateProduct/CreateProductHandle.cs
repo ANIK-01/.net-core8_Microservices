7403ce842089f00194c783b6b5a508b5f61deec7
ANIK01 <anikmd445@gmail.com>
2025-01-19 21:19:01 +0600

    !docker add container & postgres setup

--- core8-microservices/src/Services/Catalog/CatalogApi/Products/CreateProduct/CreateProductHandle.cs
+++ core8-microservices/src/Services/Catalog/CatalogApi/Products/CreateProduct/CreateProductHandle.cs
@@ -1,17 +1,33 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using CatalogApi.Models;
 
 namespace CatalogApi.Products.CreateProduct
 {
 
-    public record CreateProductCommand(string Name, List<string> Category, string Description,decimal price) 
-        : IRequest<CreateProductResult> ;
+    public record CreateProductCommand(string Name, List<string> Category, string Description,string ImageFile, decimal Price) 
+        : ICommand<CreateProductResult> ;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    public class CreateProductCommandHandler (IDocumentSession session)
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+            //Create product entity from command object
+            //save to result 
+            //return Create product result
 
-            throw new NotImplementedException();
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+
+            };
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+            return new CreateProductResult(product.Id);
         }
     }
 }
--- core8-microservices/src/Services/Catalog/CatalogApi/Program.cs
+++ core8-microservices/src/Services/Catalog/CatalogApi/Program.cs
@@ -1,10 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+builder.Services.AddMarten(Opts =>
+{
+    Opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
-
+app.MapGet("/", () => "Hello from DOCKER");
 //Configure the HTTp request pipeline
-
+app.MapCarter();
 
 app.Run();
