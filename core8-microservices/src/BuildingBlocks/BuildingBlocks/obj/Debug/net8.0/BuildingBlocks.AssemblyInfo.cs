625d174887d2eae9da29b190b6b931d5852dc915
Mohiuzzaman Anik <mohiuzzaman.anik@softrobotics.com.bd>
2025-01-21 12:19:31 +0600

    !redis deleted as redis/stack container already available

--- core8-microservices/src/BuildingBlocks/BuildingBlocks/obj/Debug/net8.0/BuildingBlocks.AssemblyInfo.cs
+++ core8-microservices/src/BuildingBlocks/BuildingBlocks/obj/Debug/net8.0/BuildingBlocks.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuildingBlocks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7403ce842089f00194c783b6b5a508b5f61deec7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79278d4dec3c2e9b1eaa14d4216ecd691dd31272")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuildingBlocks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuildingBlocks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
